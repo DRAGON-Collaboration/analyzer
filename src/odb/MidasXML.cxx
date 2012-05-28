530427c49cbda20c1acea5eb505c9115b6bfe7db
Greg Christian <gregc6923@gmail.com>
2012-05-28 12:50:06 -0700

    added midasxml stuff

---
+++ src/odb/MidasXML.cxx
@@ -0,0 +1,260 @@
+//! \file MidasXML.cxx
+//! \brief Implements MidasXML.hxx
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include "MidasXML.hxx"
+
+MidasXML::MidasXML(const char* filename):
+	fTree(0), fOdb(0)
+{
+	Init(filename);
+}
+
+MidasXML::~MidasXML()
+{
+	if(fTree) mxml_free_tree(fTree);
+}
+
+void MidasXML::Init(const char* filename)
+{
+	char err[256]; int err_line;
+	fTree = ParseFile(filename, err, sizeof(err), &err_line);
+	if(!fTree) {
+		std::cerr << "Error: Bad XML file: " << filename << ", error message: " <<
+			 err << ", error line: " << err_line << "\n";
+		return;
+	}
+	fOdb = mxml_find_node(fTree, "/odb");
+	if(!fOdb) {
+		std::cerr << "Error: no odb tag found in xml file: " << filename << ".\n";
+	}
+}
+
+MidasXML::Node MidasXML::ParseFile(const char* file_name, char *error, int error_size, int *error_line)
+{
+	char* buf, line[1000];
+	int length = 0;
+	PMXML_NODE root;
+	FILE* f;
+
+	if (error)
+		 error[0] = 0;
+
+	f = fopen(file_name, "r");
+
+	if (!f) {
+		sprintf(line, "Unable to open file \"%s\": ", file_name);
+		strlcat(line, strerror(errno), sizeof(line));
+		strlcpy(error, line, error_size);
+		return NULL;
+	}
+
+	fpos_t startPos;
+	int start_found(0);
+	while(1) {
+		if(!start_found) {
+			char str[256];
+			size_t nRead = fread(str, 1, strlen("<odb"), f);
+			fseek(f, -1*(strlen("<odb")-1), SEEK_CUR);
+			if(nRead != strlen("<odb")) {
+				sprintf(error, "Could not find \"<odb\"");
+				*error_line = __LINE__;
+				fclose(f);
+				return NULL;
+			}
+			if(!memcmp(str, "<odb", strlen("<odb"))) {
+				fseek(f, -1, SEEK_CUR);
+				fgetpos(f, &startPos);
+				start_found = 1;
+			}
+		}
+		else {
+			char str[256];
+			size_t nRead = fread(str, 1, strlen("</odb>"), f);
+			fseek(f, -1*(strlen("</odb>")-1), SEEK_CUR);
+			length++;
+			if(nRead != strlen("</odb>")) {
+				sprintf(error, "Could not find \"</odb>\"");
+				*error_line = __LINE__;
+				fclose(f);
+				return NULL;
+			}
+			if(!memcmp(str, "</odb>", strlen("</odb>"))) {
+				break;
+			}
+		}
+	}
+
+	length += strlen("</odb>");
+	fsetpos(f, &startPos);
+	buf = (char *)malloc(length+1);
+	if (buf == NULL) {
+		fclose(f);
+		sprintf(line, "Cannot allocate buffer: ");
+		strlcat(line, strerror(errno), sizeof(line));
+		strlcpy(error, line, error_size);
+		return NULL;
+	}
+
+  /* read odb portion of the file */
+	length = (int)fread(buf, 1, length, f);
+	buf[length] = 0;
+	fclose(f);
+
+	if (mxml_parse_entity(&buf, file_name, error, error_size, error_line) != 0) {
+		free(buf);
+		return NULL;
+	}
+
+	root = mxml_parse_buffer(buf, error, error_size, error_line);
+
+	free(buf);
+
+	return root;
+
+}
+
+
+bool MidasXML::Check()
+{
+	if(fTree && fOdb) return true;
+	std::cerr << "Warning: MidasXML object was initialized with a bad XML file, "
+						<<"cannot perform any further operations.\n";
+	return false;
+}
+
+namespace {
+inline std::vector<std::string> path_tokenize(const char* path) {
+	std::string strPath(path);
+	std::vector<std::string> nodes;
+	long posn (0);
+	while(strPath.find("/") < strPath.size()) {
+		posn = strPath.find("/");
+		nodes.push_back(strPath.substr(0, posn));
+		strPath = strPath.substr(posn+1);
+	}
+	nodes.push_back(strPath);
+	return nodes;
+}
+std::string get_xml_path(const char* path, const char* node_type) {
+	std::vector<std::string> nodes = path_tokenize(path);
+	std::stringstream xmlPath;
+	for(std::vector<std::string>::iterator it = nodes.begin(); it != nodes.end(); ++it) {
+		if(it != nodes.end() - 1) xmlPath << "/dir[@name=" << *it << "]";
+		else xmlPath << "/" << node_type << "[@name=" << *it << "]";
+	}
+	return xmlPath.str();
+} }
+
+MidasXML::Node MidasXML::FindKey(const char* path)
+{
+	if(!Check()) return 0;
+	Node out = mxml_find_node(fOdb, get_xml_path(path, "key").c_str());
+	if(!out) {
+		std::cerr << "Error: XML path: " << path << " was not found.\n";
+	}
+	return out;
+}
+
+MidasXML::Node MidasXML::FindKeyArray(const char* path)
+{
+	if(!Check()) return 0;
+	Node out = mxml_find_node(fOdb, get_xml_path(path, "keyarray").c_str());
+	if(!out) {
+		std::cerr << "Error: XML path: " << path << " was not found.\n";
+	}
+	return out;
+}
+
+template <typename T> T MidasXML::GetValue(const char* path, bool* success)
+{
+	T out;
+	GetValue(path, out, success);
+	return out;
+}
+
+template <typename T> void MidasXML::GetValue(const char* path, T& value, bool* success)
+{
+	if(success) *success = true;
+	Node node = FindKey(path);
+	if(!node) {
+		if(success) *success = false;
+		return;
+	}
+	if(typeid(T) != typeid(bool)) {
+		std::stringstream val;
+		val << node->value;
+		val >> value;
+	}
+	else {
+		value = (!strcmp("y", node->value)) ? true : false;
+	}
+}
+
+template <typename T> void MidasXML::GetArray(const char* path, std::vector<T>& array, bool* success)
+{
+	if(success) *success = true;
+	Node node = FindKeyArray(path);
+	if(!node) {
+		if(success) *success = false;
+		return;
+	}
+	array.clear();
+	char* pAttribute = mxml_get_attribute(node, "num_values");
+	if(!pAttribute) {
+		std::cerr << "Error: \"num_values\" attribute not found for array: " << path << "\n";
+		if(success) *success = false;
+		return;
+	}
+	int size = atoi(pAttribute);
+	for(int i=0; i< size; ++i) {
+		std::stringstream valPath;
+		valPath << "/value[" << i+1 << "]";
+		Node valNode = mxml_find_node(node, valPath.str().c_str());
+		if(!valNode) {
+			std::cerr << "Error: Unable to find value node for array index " << i << "\n";
+			continue;
+		}
+
+		T value;
+		if(typeid(T) != typeid(bool)) {
+			std::stringstream val;
+			val << valNode->value;
+			val >> value;
+		}
+		else {
+			value = (!strcmp("y", valNode->value)) ? true : false;
+		}
+		array.push_back(value);
+
+	}
+}
+
+template <typename T> std::vector<T> MidasXML::GetArray(const char* path, bool* success)
+{
+	std::vector<T> out;
+	GetArray<T>(path, out, success);
+	return out;
+}
+
+#ifdef MIDAS_XML_TESTING
+int main() {
+	MidasXML mxml("run23822.mid");
+	MidasXML::Node loc = mxml.FindKey("System/Clients/19764/Host");
+	if(!loc) return 1;
+	
+	int i = -1;
+	std::vector<short> peds;
+	mxml.GetArray("Equipment/gTrigger/Variables/Pedestals", peds);
+	for(int i=0; i< peds.size(); ++i) {
+		std::cout << "peds[" << i << "]: " << peds[i] << "\n";
+	}
+}
+#endif
---
+++ src/odb/MidasXML.hxx
@@ -0,0 +1,90 @@
+//! \file MidasXML.hxx
+//! \brief Defines a class to parse MIDAS XML files from the ODB.
+#ifndef MIDAS_XML_HXX
+#define MIDAS_XML_HXX
+#include <vector>
+#include "mxml.h"
+
+/// \brief Class to parse MIDAS ODB XML files.
+class MidasXML {
+public:
+	 /// Pointer to an XML node.
+	 typedef PMXML_NODE Node;
+
+private:
+	 /// Pointer to the entire XML tree contained within a file
+	 Node fTree;
+	 /// Pointer to the ODB portion of fTree
+	 Node fOdb;
+
+public:
+	 /// \brief Read data from an XML file
+	 /// \details Parses a file containing XML data to fill fTree and fOdb.
+	 /// Can handle either a dedicated \c .xml file or a \c .mid (or any other) file containing
+	 /// the XML data as a subset.
+	 /// \note Memory is allocated only to fTree, fOdb and any other subnodes used later
+	 /// simply refer to memory locations relative to fTree.
+	 MidasXML(const char* filename);
+
+	 /// Frees resources allocated to fTree
+	 ~MidasXML();
+
+	 /// \brief Find the node location of a specific key element within the xml file
+	 /// \param [in] path String specifying the "directory" path of the element, e.g.
+	 /// "Equipment/gTrigger/Variables/Pedestals".
+	 Node FindKey(const char* path);
+
+	 /// \brief Find the node location of a specific keyarray element within the xml file
+	 /// \param [in] path String specifying the "directory" path of the element, e.g.
+	 /// "Equipment/gTrigger/Variables/Pedestals".
+	 Node FindKeyArray(const char* path);
+
+	 /// \brief Get the value of key element
+	 /// \tparam The type of the key element (e.g. int, float, string, ...)
+	 /// \param [in] path String specifying the "directory" path of the element, e.g.
+	 /// "Equipment/gTrigger/Variables/Pedestals"
+	 /// \param [out] success Sets the value to \c true if \e path was valid, \c false otherwise.
+	 ///  Passing a NULL value to the function disables the error reporting
+	 /// \returns The value of the element specified by \e path
+	 template <typename T> T GetValue(const char* path, bool* success = 0);
+
+	 /// \brief Get the value of key element
+	 /// \tparam The type of the key element (e.g. int, float, string, ...)
+	 /// \param [in] path String specifying the "directory" path of the element, e.g.
+	 /// "Equipment/gTrigger/Variables/Pedestals"
+	 /// \param [out] value Set to the value of the element specified by \e path
+	 /// \param [in] success Sets the value to \c true if \e path was valid, \c false otherwise.
+	 ///  Passing a NULL value to the function disables the error reporting
+	 template <typename T> void GetValue(const char* path, T& value, bool* success = 0);
+
+	 /// \brief Get the values of an array of key elements
+	 /// \tparam The type of the key element (e.g. int, float, string, ...)
+	 /// \param [out] path String specifying the "directory" path of the element, e.g.
+	 /// "Equipment/gTrigger/Variables/Pedestals"
+	 /// \param [out] success Sets the value to \c true if \e path was valid, \c false otherwise.
+	 ///  Passing a NULL value to the function disables the error reporting
+	 /// \returns A vector of all array values
+	 template <typename T> std::vector<T> GetArray(const char* path, bool* success = 0);
+
+	 /// \brief Get the values of an array of key elements
+	 /// \tparam The type of the key element (e.g. int, float, string, ...)
+	 /// \param [in] path String specifying the "directory" path of the element, e.g.
+	 /// "Equipment/gTrigger/Variables/Pedestals"
+	 /// \param [out] array Set to a vector of all array values
+	 /// \param [out] success Sets the value to \c true if \e path was valid, \c false otherwise.
+	 ///  Passing a NULL value to the function disables the error reporting
+	 template <typename T> void GetArray(const char* path, std::vector<T>& array, bool* success = 0);
+
+private:
+	 /// \brief Helper initialization function called by the constructor.
+	 void Init(const char* filename);
+	 /// \brief Helper function to parse a file containing XML data and set fTree and fObd
+	 /// \note Most of the implementation was a paraphrase of mxml_parse_file() in midas.c,
+	 /// extended to handle files that contain the XML data only as a subset (i.e. MIDAS files).
+	 Node ParseFile(const char* file_name, char *error, int error_size, int *error_line);
+	 /// \brief Check if fTree and fOdb are non-null
+	 bool Check();
+};
+
+
+#endif
