d5bffebea686207422a10de72e3d0844ee16ab18
Greg Christian <gregc6923@gmail.com>
2012-09-27 12:53:10 -0700

    Added Head.* files to repository

---
+++ src/dragon/Head.cxx
@@ -0,0 +1,71 @@
+/// \file Head.cxx
+/// \author G. Christian
+/// \brief Implements Head.hxx
+#include "Head.hxx"
+
+
+// ========== Class dragon::Head ========== //
+
+dragon::Head::Head() :
+	io32(), v792(), v1190(), header(), bgo()
+{
+	reset();
+}
+
+void dragon::Head::reset()
+{
+	io32.reset();
+	v792.reset();
+	v1190.reset();
+	bgo.reset();
+	const midas::Event::Header temp = { 0, 0, 0, 0, 0 };
+	header = temp;
+}
+
+void dragon::Head::set_variables(const char* odb)
+{
+	/*!
+	 * \param [in] odb_file Name of the ODB file
+	 * \note Passing \c "online" looks at the online ODB.
+	 */
+	bgo.variables.set(odb);
+}
+
+void dragon::Head::unpack(const midas::Event& event)
+{
+	/*!
+	 * \param [in] event Reference to a Midas event structure
+	 *
+	 * Here is where the low-level work is done to take bitpacked data read directly
+	 * from a MIDAS file and convert it into the corresponding measurement values
+	 * of various ADC modules.
+	 *
+	 * In the specific case of unpacking the dragon::Head VME data, we delegate
+	 * the work to the unpack() routines of the relevant vme::* classes.
+	 *
+	 * \note Recompile with <c> report = true </c> to print warning messages
+	 *  for missing banks
+	 *
+	 * \todo Don't hard code bank names
+	 */
+	const bool report = false;
+	io32.unpack (event, "VRTH", report);
+	v792.unpack (event, "ADC0", report);
+	v1190.unpack(event, "TDC0", report);
+	event.CopyHeader(header);
+}
+
+void dragon::Head::calculate()
+{
+	/*!
+	 * This is where the raw data recorded in VME fields is mapped to a more
+	 * abstract location in a detector class. This covers a wide range of parameter
+	 * calculation, from, e.g. simply mapping ADC channels to corresponding (uncalibrated)
+	 * detector signals, up to calculation of abstract physics quantities that depend on multiple
+	 * calibrated detector signals.
+	 *
+	 * In the specific implementation, we delegate to functions in the dragon::Bgo class.
+	 */
+	bgo.read_data(v792, v1190);
+	bgo.calculate();
+}
---
+++ src/dragon/Head.hxx
@@ -0,0 +1,65 @@
+/// \file Head.hxx
+/// \author G. Christian
+/// \brief Defines classes for DRAGON head (gamma-ray) detectors.
+#ifndef DRAGON_HEAD_HXX
+#define DRAGON_HEAD_HXX
+#include "midas/Event.hxx"
+#include "vme/V1190.hxx"
+#include "vme/V792.hxx"
+#include "vme/IO32.hxx"
+#include "Bgo.hxx"
+
+namespace dragon {
+
+/// Collection of all head detectors and VME modules
+class Head {
+
+public:
+
+#ifdef DISPLAY_MODULES
+	/// IO32 FPGA
+	vme::IO32 io32; //#
+
+	/// CAEN V792 QDC
+	vme::V792 v792; //#
+
+	/// CAEN V1190 TDC
+	vme::V1190 v1190; //#
+#else
+	/// IO32 FPGA
+	vme::IO32 io32; //!
+
+	/// CAEN V792 QDC
+	vme::V792 v792; //!
+
+	/// CAEN V1190 TDC
+	vme::V1190 v1190; //!
+#endif
+
+	/// Midas event header
+	midas::Event::Header header; //#
+
+	/// Bgo array
+	dragon::Bgo bgo;
+
+public:
+	/// Initializes data values
+	Head();
+
+	/// Sets all data values to vme::NONE or other defaults
+	void reset();
+
+	///  Reads all variable values from an ODB (file or online)
+	void set_variables(const char* odb_file);
+
+	/// Unpack raw data into VME modules
+	void unpack(const midas::Event& event);
+
+	/// Calculate higher-level data for each detector, or across detectors
+	void calculate();
+};
+
+} // namespace dragon
+
+
+#endif
