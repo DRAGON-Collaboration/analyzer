60339310da99521d30cd403e38bd4d4792450bd8
Greg Christian <gregc6923@gmail.com>
2012-09-28 15:23:38 -0700

    Doxygen updates for the ROOTBEER stuff

--- src/rootbeer/DragonRootbeer.cxx
+++ src/rootbeer/DragonRootbeer.cxx
@@ -1,19 +1,62 @@
-//! \file DragonRootbeer.cxx
-//! \author G. Christian
-//! \brief Implementation of DRAGON user functions for ROOTBEER.
-#include "DragonRootbeer.hxx"
-#include "utils/Error.hxx"
-#include "dragon/definitions.h"
+/// \file DragonRootbeer.cxx
+/// \author G. Christian
+/// \brief Implementation of rb::BufferSource::New() and rb::Rint::RegisterEvents()
 
-rb::BufferSource* rb::BufferSource::New() {
-  return new rb::Midas();
+// ROOTBEER includes //
+#include "Rint.hxx"
+#include "Buffer.hxx"
+
+// DRAGON includes //
+#include "utils/definitions.h"
+#include "MidasBuffer.hxx"
+#include "DragonEvents.hxx"
+#include "DragonRootbeer.hxx"
+
+
+rb::BufferSource* rb::BufferSource::New()
+{
+	/*!
+	 * From http://trshare.triumf.ca/~gchristian/rootbeer/doc/html/classrb_1_1_buffer_source.html
+	 *
+	 * This is used as a generic way to get a pointer to an instance of a class derived from BufferSource,
+	 * when the specific derived class is unknown <i>a priori</i>.  This function should be implemented by users
+	 * such that it returns a pointer to the specific class they want to use for reading and unpacking data. Example:
+	 * \code
+	 * BufferSource* New() {
+	 *     return new MyBufferSource();
+	 * }
+	 * \endcode
+	 * \returns A pointer to a \c new instance of a class derived from BufferSource.
+	 *
+	 * In this specific instance, we return a pointer to a \c new rootbeer::MidasBuffer
+	 */ 
+	
+  return new rootbeer::MidasBuffer();
 }
 
-void rb::Rint::RegisterEvents() {
-  // Register events here //
-  RegisterEvent<CoincidenceEvent>(COINCIDENCE_EVENT, "CoincidenceEvent");
-	RegisterEvent<GammaEvent>(GAMMA_EVENT, "GammaEvent");
-	RegisterEvent<HeavyIonEvent>(HI_EVENT, "HeavyIonEvent");
+void rb::Rint::RegisterEvents()
+{
+	/*!
+	 * From http://trshare.triumf.ca/~gchristian/rootbeer/doc/html/classrb_1_1_buffer_source.html :
+	 *
+	 * Registers all event processors in the program.
+	 *
+	 * This needs to be implemented by users to account for the different event processors
+	 * they want to use in the program. See rb::Event documentation for more information.
+	 *
+	 * In this specific case, we registers event processors for:
+	 */
+
+	/// - Gamma singles [ rootbeer::GammaEvent ]: "GammaEvent"
+	RegisterEvent<rootbeer::GammaEvent> (DRAGON_HEAD_EVENT, "GammaEvent");
+
+	/// - Heavy-ion singles [ rootbeer::HeavyIonEvent ]: "HeavyIonEvent"
+	RegisterEvent<rootbeer::HeavyIonEvent> (DRAGON_TAIL_EVENT, "HeavyIonEvent");
+
+	/// - Coincidences [ rootbeer::CoincEvent ]: "CoincEvent"
+  RegisterEvent<rootbeer::CoincEvent> (DRAGON_COINC_EVENT, "CoincEvent");
+
+	/// \todo Include scaler event processors
 }
 
 
