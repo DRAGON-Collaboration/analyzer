351c41aed096215e08b720d46158880e76be2030
Dragon Group test setup <dragon@ladd06.triumf.ca>
2012-10-24 17:38:05 -0700

    Doxygen

--- src/utils/Valid.hxx
+++ src/utils/Valid.hxx
@@ -38,6 +38,9 @@ const int32_t NO_DATA = -1;
 
 }
 
+
+namespace utils {
+
 template <class T>
 inline bool is_valid(T* const tArray, int len)
 {
@@ -790,5 +793,8 @@ inline void reset_data (T0& t0, T1& t1, T2& t2, T3& t3, T4& t4, T5& t5, T6& t6, 
   t28 = (T28)dragon::NO_DATA;
   t29 = (T29)dragon::NO_DATA;
 }
+
+} // namespace utils
+
 
 #endif
