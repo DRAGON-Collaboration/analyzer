c39e028d5abfe08ef7e166545aae54f935be1fbe
Greg Christian <gregc6923@gmail.com>
2012-11-09 12:14:57 -0800

    Documentation mainpage updates

--- src/dragon/Coinc.cxx
+++ src/dragon/Coinc.cxx
@@ -202,7 +202,7 @@ void dragon::Coinc::calculate()
 
 	\section using For Users
 
-	\subsection Rootana online analyzer
+	\subsection rootana Rootana Online Analyzer
 
 	This package includes a library to interface the DRAGON analysis codes with MIDAS's
 	<a href=http://ladd00.triumf.ca/~olchansk/rootana/>rootana</a> system,
@@ -217,7 +217,170 @@ void dragon::Coinc::calculate()
 	allow for easy and quick changes to be made to the available visualization, even for users who are completely
 	inexperienced with C++.
 
+	Histograms are created at runtime (\c anaDragon program start) by parsing a text file that contains histogram
+	definitions. By default, histograms are created from the file <tt>${DR_HOME}/histos.dat</tt>, where \c ${DR_HOME}
+	is the location of the DRAGON analyzer package. However, you can alternatively specify a different histogram
+	definition file by invoking the \c -histos flag at program start:
+	\code
+	./anaDragon -histos my_histograms.dat
+	\endcode
+
+	Note that the default histogram file (or the one specified with the \c -histos flag, creates histograms that
+	are available both online and offline, that is, they are viewable online using the roody interface, and they
+	are also saved to a .root file at the end of the run. It is also possible to specify a separate set of histograms
+	for online viewing \e only. To do this, invoke the \c -histos0 flag:
+	\code
+	./anaDragon -histos0 histograms_i_only_care_to_see_online.dat
+	\endcode
+
+	Now that you know how to specify what file to read the histogram definitions from, you should probably also
+	know how to write said file. The basic way in which the files are parsed is by using a set of "key" codes
+	that tell the parser to look in the lines below for the appropriate information for defining the corresponding
+	ROOT object. The "keys" available are as follows:
 	
+	-# DIR: Create a directory.\n\n
+	The succeding line should contain the full path of the directory as plain text.\n
+	For example:
+  \code
+  DIR:
+  histos/gamma
+  \endcode
+  This would create a new directory "histos", with "gamma" as a subdirectory. Note that if you later do
+  \code
+  DIR:
+  histos/hion
+  \endcode
+  the "histos" directory would not be re-created; instead, "hion" would be added as a new subdirectory. When
+  you specify a directory, any histogram definitions succeding it will be members of that directory, until another
+  directory line is encountered. If any histograms are defined before the first "DIR:" command, they will belong to
+  the "top level" directory (e.g. in a TFile, their owning directory will be the TFile itself).
+	\n\n
+	-# TH1D: Create a 1d histogram.\n\n
+	Should be succeded by two lines: 1) the argument to a TH1D constructor, and 2)
+	the parameter to display in the histogram.\n
+	Example:
+	\code
+  TH1D:
+        ("bgo_e0", "Bgo energy, channel 0 [singles]", 256, 0, 4096)
+        rootana::gHead.bgo.ecal[0]
+	\endcode
+	(note that the indentations are only for readability, not required). This creates a histogram with name "bgo_e0",
+	title "Bgo energy, channel 0 [singles]", and ranging from 0 to
+	4096 with 256 bins. The parameter to be displayed in the histogram would be rootana::gHead.bgo.ecal[0].
+	\n\n
+	-# TH2D: Create a 2d histogram.\n\n
+	Succeding lines are in the same sprit as the 1d case, except a third line is
+	aded to specify the y-axis parameter.\n
+	Example:
+	\code
+  TH2D:
+      ("bgo_e1_e0", "Bgo energy 1 vs. 0 [coinc]", 256, 0, 4096, 256, 0, 4096)
+      rootana::gCoinc.head.bgo.ecal[1]
+      rootana::gCoinc.head.bgo.ecal[0]
+	\endcode
+	Creates a histogram of rootana::gCoinc.head.bgo.ecal[1] [x-axis] vs. rootana::gCoinc.head.bgo.ecal[0] [y-axis].
+	\n\n
+	-# TH3D Create a 3d histogram.\n\n
+	Following the same pattern as 1d and 2d - add a third line to specify the
+	z-axis parameter.
+	\n\n
+	-# SUMMARY: Create a "summary" histogram, that is a histogram which displays information on multiple channels
+	at once, like this:\n\n
+	\image html summary.png
+	\n\n
+	Note that sumary histograms are currently only available for arrays, that is, where each y-axis bin corresponds
+	to a different array index. If you need to display summary information for parameters not contained in an array,
+	you will have to define the histogram manually in C++. However, in nearly all cases where summary imformation
+	might be desired, the parameters are already contained in an array anyway.
+	\n\n
+	The lines following a "SUMMARY:" command are 1) Constructor for a 1d histogram; this will define the x-axis
+	binning and set the histogram name/title; 2) The name of an array that you want to display in the histogram;
+	and 3) The number of y-axis bins (should be equal to the length of the array).\n
+	Example:
+	\code
+  SUMMARY:
+      ("bgo_q", "Bgo energies [singles]", 256, 0, 4096)
+      rootana::gHead.bgo.ecal
+      30
+	\endcode
+	This would create a summary histogram of the 30 BGO detector energies contained in the rootana::gHead.bgo.ecal
+	array (bgo.ecal[0] -> bgo.ecal[29]).
+	\n\n
+	-# CUT: Defines a cut.\n\n
+	This defines a "cut" or "gate" condition that will be applied to the histogram defined directly before it.
+	The cut condition is specified as a logical condition consisting of rootana::Cut derived classes. For more information
+	on how to define a cut, see the code documentation of the Cut.hxx source file and links therein.\n
+	As a simple example:
+	\code
+	TH1D:
+      ("bgo_e0", "Bgo energy, ch 0 [singles]", 256, 0, 4096)
+      rootana::gHead.bgo.ecal[0]
+  CUT:
+      Less(gHead.bgo.ecal[0], 2000) && Greater(gHead.bgo.ecal[0], 100)
+  \endcode
+	will create the "bgo_e0" histogram displaying bgo.ecal[0], with the condition that bgo.ecal[0] be greater-than
+	100 and less-than 2000.\n\n
+	Note that you can also create/use 2d polygon cuts, either by specifying the parameters and (closed) polygon
+	points:
+	\code
+	CUT:
+      Cut2D(gHead.bgo.q[0], gHead.bgo.q[1], -2,-2, -2,2, 2,2, 2,-2, -2,-2)
+  \endcode
+	This would create the following cut:\n\n
+	\image html cut.png
+	It is also possible to pre-define and then re-use graphical cut using the "CMD:" option (see next).
+	\n\n
+	-# CMD: Evaluates a series of commands in CINT.\n\n
+	The succeeding lines are a series of C++ statements to be evaluated literally in the CINT interpreter, closed
+	by a line containing only "END".
+	Any objects created within these statements will become available for use in future commands throughout
+	the definition file. Note that each file should only contain one "CMD:" statement (if it contains any), and
+	that it should be the first "active" code within the file.\n
+	As an example, we could use "CMD:" to define a graphical cut:
+	\code
+  CMD:
+      TCutG cutg("cutTest",7);
+      cutg.SetVarX("");
+      cutg.SetVarY("");
+      cutg.SetTitle("Graph");
+      cutg.SetFillColor(1);
+      cutg.SetPoint(0,37.8544,70.113);
+      cutg.SetPoint(1,25.5939,41.5819);
+      cutg.SetPoint(2,54.9042,28.0226);
+      cutg.SetPoint(3,83.0651,35.3672);
+      cutg.SetPoint(4,81.5326,53.1638);
+      cutg.SetPoint(5,59.3103,79.1525);
+      cutg.SetPoint(6,37.8544,70.113);
+  END
+	\endcode
+	This cut is then available in future satements; for example to create a Cut2D object to be applied to a histogram
+	\code
+  SUMMARY:
+      ("bgo_ecal", "Bgo energies [singles]", 256, 0, 4096)
+       rootana::gHead.bgo.ecal
+      30
+  CUT:
+      Cut2D(gHead.bgo.ecal[0], gHead.bgo.ecal[1], cutg)
+  \endcode
+  The above example would apply the graphical cut "cutg" defined in the "CMD:" statement to the "bgo_ecal" histogram.
+
+	With combinations of the above commands, you should hopefully be able to define any histogram-related objects you'll
+	need in the online analyzer without ever having to touch the source code. A few more notes about histogram definition
+	files:
+
+	- Whitespace is ignored, though good indention improves readability immensely.
+	\n\n
+	- The \c # character denotes a comment, all characters on the same line coming after a \c # are ignored:
+	\code
+	# IGNORE THIS HEADER STATEMENT #
+	TH1D: #Ignore this descriptive comment also...
+	\endcode
+	\n\n
+	- The runtime parsing is done using CINT via the gROOT->ProcessLineFast command. This has very little native
+	error handling. Some support has been added in the DRAGON analyzer to "gracefully" handle common errors, for
+	example skipping the current definition and moving onto others, but alerting the user. If you find a case where
+	you feel the error handling could be improved (in particular, where a mistake in the script file causes a crash
+	or un-reported failure), do not hesitate to alert the developers.
 
 
 	\section developers For Developers
