4ca42fbe13b21b79b6790e7a61a0901882fe7944
Dragon Group test setup <dragon@ladd06.triumf.ca>
2012-11-14 14:10:29 -0800

    Added runtime check to anaDragon for PRIVATE=public (necessar for hist parsing)

--- src/rootana/anaDragon.cxx
+++ src/rootana/anaDragon.cxx
@@ -6,10 +6,36 @@
  */
 #include <signal.h>
 #include <memory>
+#include <string>
+#include <iostream>
 #include "Callbacks.hxx"
+
+#ifndef ROOTANA_STRINGIFY
+#define ROOTANA_STRINGIFY_MACRO(S) ROOTANA_STRINGIFY (S)
+#define ROOTANA_STRINGIFY(S) #S
+#endif
+
+
+bool check_public()
+{
+	std::string priv = ROOTANA_STRINGIFY_MACRO(PRIVATE);
+	bool ret = true;
+
+	if(priv != "public") {
+		std::cerr << "Error: To use the histogram parser, anaDragon must be compiled with -DPRIVATE=public\n"
+							<< "Please change the appropriate lines near the top of your Makefile and re-compile.\n\n";
+		ret = false;
+	}
+
+	return ret;	
+}
+	
 
 int main(int argc, char *argv[])
 {
+	if(!check_public())
+		return 1;
+
 	setbuf(stdout,NULL);
 	setbuf(stderr,NULL);
  
