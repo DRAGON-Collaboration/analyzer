ddbbaddd0870e48fe4340d45cb12f80b1a17af21
Devin Connolly <dconnolly@triumf.ca>
2016-08-30 12:13:07 -0700

    removed spurious whitespace in RootAnalysis.cxx

--- src/utils/RootAnalysis.cxx
+++ src/utils/RootAnalysis.cxx
@@ -2117,7 +2117,9 @@ UDouble_t dragon::StoppingPowerCalculator::CalculateEbeam(TGraph** plot)
 	for(Int_t i=0; i< GetNmeasurements(); ++i) {
 
 		double md1err = fMd1[i].GetErrLow() < fMd1[i].GetErrHigh() ?
-																					fMd1[i].GetErrHigh() : fMd1[i].GetErrLow(); pres[i] = fPressures[i];
+																					fMd1[i].GetErrHigh() : fMd1[i].GetErrLow();
+
+		pres[i] = fPressures[i];
 		energy[i] = CalculateEnergy(fMd1[i].GetNominal(), md1err, fBeamCharge, fBeamMass, fMd1Constant.GetNominal(), 0);
 	}
 	//
@@ -2172,6 +2174,7 @@ UDouble_t dragon::StoppingPowerCalculator::CalculateEpsilon(TGraph** plot, UDoub
 
 		double md1err = fMd1[i].GetErrLow() < fMd1[i].GetErrHigh() ?
 																					fMd1[i].GetErrHigh() : fMd1[i].GetErrLow();
+
 		dens[i] = CalculateDensity(fPressures[i], UDouble_t(fTargetLength.GetNominal(), 0), fNmol, fTemp);
 		energy[i] = CalculateEnergy(fMd1[i].GetNominal(), md1err, fBeamCharge, fBeamMass, fMd1Constant.GetNominal(), 0);
 	}
