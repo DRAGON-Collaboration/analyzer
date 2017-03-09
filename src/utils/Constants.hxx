dd3fa4ad548adf81f13899ca38f7db02f6e4bd05
Devin Connolly <dconnolly@triumf.ca>
2017-03-09 13:43:55 -0800

    minor edits to documentation of Constants.h

--- src/utils/Constants.hxx
+++ src/utils/Constants.hxx
@@ -12,21 +12,21 @@
 namespace dragon {
 
 /// Class containing useful physical constants as static members
+
 /*! Example use:
  *  \code
  *  std::cout << "The electron charge in coulombs is: " << dragon::Constants::ElectronCharge() << "\n";
  *  \endcode
  *  \note All values taken from 2014 CODATA values:
  *  <a href="http://physics.nist.gov/cuu/Constants/index.html">http://physics.nist.gov/cuu/Constants/index.html</a>
- *  \These values should be updated in 2018 the CODATA values are adjusted to the revised SI units. See
- *  \http://www.bipm.org/en/news/full-stories/2015-01/si-roadmap.html for details.
+ *  These values should be updated in 2018 when the CODATA values are adjusted to adhere to the
+ *  <a href="http://www.bipm.org/en/news/full-stories/2015-01/si-roadmap.html">revision of the International System of Units (SI)</a>.
  *
  *	\todo
- *  consider adding functionality to read and parse a <a href="http://physics.nist.gov/cuu/Constants/Table/allascii.txt">text file</a>
- *  containing physical constant data (much like
- *  \code TAtomicMass \endcode
- *  \endtodo
+ *  consider adding functionality (similar to dragon::TAtomicMassTable::ParseFile ) to read and parse a
+ *  <a href="http://physics.nist.gov/cuu/Constants/Table/allascii.txt">text file</a> containing physical constant data.
  */
+
 class Constants {
 public:
 
@@ -34,10 +34,10 @@ public:
 	static const double Avogadro()   { return 6.022140857e23; }
 
 	/// AMU in MeV/c^2
-	static const double AMU()   { return 931.4940954; }
+	static const double AMU()        { return 931.4940954; }
 
 	/// AMU uncertainty in MeV/c^2
-	static const double AMUUnc() { return 0.0000057; } //MeV
+	static const double AMUUnc()     { return 0.0000057; }
 
 	/// AMU in kilograms
 	static const double AMUkg() { return 1.660538921e-27; }
@@ -60,14 +60,14 @@ public:
 	/// Boltzmann constant uncertainty
 	static const double BoltzUnc() { return 0.0000050e-5; }
 
-	/// Electron charge in A*s \note Implemented in TMath::Qe(), but outdated
+	/// Electron charge in A*s \note Implemented in ROOT's TMath::Qe(), but its value is outdated
 	static const double ElectronCharge() { return 1.6021766208e-19; }
 
 	/// Electron charge uncertainty
 	static const double ElectronChargeUnc() { return 0.0000000098e-19; }
 
 	/// Electron charge squared in MeV*fm
-	static const double QeSquared() { return Alpha()*HbarC(); } // MeV*fm
+	static const double QeSquared() { return Alpha()*HbarC(); }
 
 	/// Electron mass in MeV/c^2 0.5109989461(31)
 	static const double ElectronMass() { return 0.5109989461; }
@@ -79,25 +79,25 @@ public:
 	static const double Eps0() { return 1/(pow(TMath::C(),2)*Mu0())*ElectronCharge()*1e-9; }
 
 	/// Hbar*c 197.3269788(12) MeV*fm
-	static const double HbarC() { return 197.3269788; } // MeV*fm
+	static const double HbarC() { return 197.3269788; }
 
 	/// Hbar*c uncertainty
-	static const double HbarCUnc() { return 0.0000012; } // MeV*fm
+	static const double HbarCUnc() { return 0.0000012; }
 
 	/// vacuum permeability N*A^{-2}
 	static const double Mu0() { return 4*TMath::Pi()*1e-7; }
 
 	/// Loschmidt constant at 273.15 K and 101.325 kPa 2.6867811(15)e19 cm^-3
-	static const double Losch() { return 2.6867811e19; } //cm^-3
+	static const double Losch() { return 2.6867811e19; }
 
 	/// Loschmidt constant uncertainty
 	static const double LoschUnc() { return 0.0000015; }
 
 	/// NIST normal temperature in K
-	static const double T_std() { return 293.15; } //K
+	static const double T_std() { return 293.15; }
 
 	/// standard atm in Torr
-	static const double P_std() { return 760; } //Torr
+	static const double P_std() { return 760; }
 
 };
 
