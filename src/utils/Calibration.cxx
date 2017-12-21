ebc19d913fb2daa676636b1e550612a397e74642
elzoido238 <dconnolly@triumf.ca>
2017-12-21 11:09:07 -0800

    housekeeping in Calibration.cxx

--- src/utils/Calibration.cxx
+++ src/utils/Calibration.cxx
@@ -31,7 +31,7 @@ dutils::DsssdCalibrator::DsssdCalibrator(TTree* t, midas::Database* db):
 	/// \param db Pointer to a database containing the variables with which
 	///  the DSSSD ecal data in _t_ were calculated.
 	if(!fDb) return;
-	Double_t slopes[32], offsets[32];
+	Double_t slopes[NDSSSD], offsets[NDSSSD];
 	db->ReadArray("/dragon/dsssd/variables/adc/slope",  slopes, NDSSSD);
 	db->ReadArray("/dragon/dsssd/variables/adc/offset", offsets, NDSSSD);
 	for(Int_t i=0; i< NDSSSD; ++i) {
@@ -72,7 +72,7 @@ void dutils::DsssdCalibrator::DrawSummaryCal(Option_t* opt)
 	fTree->SetBranchAddress("tail", &tail);
 	for(Long_t evt = 0; evt < fTree->GetEntries(); ++evt) {
 		fTree->GetEntry(evt);
-		for(Int_t i=0; i< NDSSSD; ++i) {
+		for(Int_t i = 0; i < NDSSSD; ++i) {
 			Double_t val = tail->dsssd.ecal[i]*fParams[i].slope + fParams[i].offset;
 			fHdcal->Fill(i, val);
 		}
@@ -84,8 +84,8 @@ void dutils::DsssdCalibrator::DrawFrontCal(Option_t* opt)
 {
 	/// \param Opt Drawing option for the displayed histogram
 	if(!fTree) return;
-	// if(!(gDirectory->Get("fHdcal")))
-	// 	DrawSummaryCal("goff");
+	if(!(gDirectory->Get("fHdcal")))
+		DrawSummaryCal("goff");
 
 	fFrontcal = (TH1D *)fHdcal->ProjectionY("frontcal",0,15,opt);
 	fFrontcal->Draw(opt);
@@ -93,7 +93,7 @@ void dutils::DsssdCalibrator::DrawFrontCal(Option_t* opt)
 
 dutils::DsssdCalibrator::Param_t dutils::DsssdCalibrator::GetParams(Int_t channel) const
 {
-	if((UInt_t)channel > 31) {
+	if((UInt_t)channel > NDSSSD - 1) {
 		std::cerr << "Invalid channel " << channel << ", valid range is [0, 31].\n";
 		Param_t junk = {0,0};
 		return junk;
@@ -103,7 +103,7 @@ dutils::DsssdCalibrator::Param_t dutils::DsssdCalibrator::GetParams(Int_t channe
 
 Double_t dutils::DsssdCalibrator::GetPeak(Int_t channel, Int_t peak) const
 {
-	if((UInt_t)channel > 31) {
+	if((UInt_t)channel > NDSSSD - 1) {
 		std::cerr << "Invalid channel " << channel << ", valid range is [0, 31].\n";
 		return 0;
 	}
@@ -123,7 +123,7 @@ std::vector<Double_t> dutils::DsssdCalibrator::FindPeaks(TH1* hst, Double_t sigm
 	Int_t npeaks = spectrum.GetNPeaks();
 	std::vector<int> indx(npeaks);
 	TMath::Sort(npeaks, spectrum.GetPositionX(), &indx[0], kFALSE);
-	for(Int_t i=0; i< npeaks; ++i) {
+	for(Int_t i = 0; i < npeaks; ++i) {
 		peaks.push_back(*(spectrum.GetPositionX() + indx[i]));
 	}
 	return peaks;
@@ -146,8 +146,8 @@ Int_t dutils::DsssdCalibrator::Run(Double_t pklow, Double_t pkhigh, Double_t sig
 	nbins /= 2;
 	TH1F hpeaks("hpeaks", "", nbins, pklow, pkhigh);
 	Int_t retval = 0;
-	for(Int_t i=0; i< 32; ++i) {
-		char buf[256];
+	for(Int_t i = 0; i < NDSSSD; ++i) {
+		char buf[16];
 		sprintf(buf, "dsssd.ecal[%d]", i);
 		fTree->Project("hpeaks", buf, "", "goff");
 		std::vector<Double_t> peaks = FindPeaks(&hpeaks, sigma, threshold);
@@ -157,7 +157,7 @@ Int_t dutils::DsssdCalibrator::Run(Double_t pklow, Double_t pkhigh, Double_t sig
 		} else {
 			++retval;
 		}
-		for(int j=0; j<3; ++j)
+		for(int j = 0; j < 3; ++j)
 			fPeaks[i][j] = peaks[j];
 
 		FitPeaks(i);
@@ -168,7 +168,7 @@ Int_t dutils::DsssdCalibrator::Run(Double_t pklow, Double_t pkhigh, Double_t sig
 
 void dutils::DsssdCalibrator::FitPeaks(Int_t ch)
 {
-	if((UInt_t)ch > 31) return;
+	if((UInt_t)ch > NDSSSD - 1) return;
 	const Double_t alphaEnergies[3] = {5.15659,5.48556,5.80477}; /* Alpha energies in MeV. */
 	const Double_t dLayer = 0.0005; /* Deadlayer thickness in mm. */
 	const Double_t aEloss[3] = {138.080,133.060,128.546}; /* Stopping Powers in MeV/mm. */
@@ -178,7 +178,7 @@ void dutils::DsssdCalibrator::FitPeaks(Int_t ch)
 		aEnergy[i] = alphaEnergies[i] - aEloss[i]*dLayer;
 
 	TGraph gr(3);
-	for(Int_t i=0; i< gr.GetN(); ++i) {
+	for(Int_t i = 0; i < gr.GetN(); ++i) {
 		gr.SetPoint(i, GetPeak(ch, i), aEnergy[i]);
 	}
 	TFitResultPtr result = gr.Fit("pol1", "qns");
@@ -201,7 +201,7 @@ void dutils::DsssdCalibrator::PrintResults(const char* outfile)
 	}
 
 	fprintf(f, "Channel\tSlope\tOffset\n");
-	for(Int_t i=0; i< 32; ++i) {
+	for(Int_t i = 0; i < NDSSSD; ++i) {
 		fprintf(f, "%2d\t%.6g\t%.6g\n", i, GetParams(i).slope, GetParams(i).offset);
 	}
 
@@ -222,7 +222,7 @@ void dutils::DsssdCalibrator::PrintOdb(const char* outfile)
 		}
 	}
 
-	for(Int_t i=0; i< 32; ++i) {
+	for(Int_t i = 0; i < NDSSSD; ++i) {
 		fprintf(f, "odbedit -c \"set /dragon/dsssd/variables/adc/slope[%d] %.6g\"\n", i, GetParams(i).slope);
 		fprintf(f, "odbedit -c \"set /dragon/dsssd/variables/adc/offset[%d] %.6g\"\n", i, GetParams(i).offset);
 	}
